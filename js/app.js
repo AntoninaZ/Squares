e764c3ff902ab8021871d47ac6d4292b8c678669
Antonina Zhuk <zuch21@gmail.com>
2017-06-26 08:55:36 +0300

    added some change

--- js/app.js
+++ js/app.js
@@ -1,9 +1,4 @@
-var Square = function(element, clickCounter) {
-	
-	this.paint = function() {
-		element.classList.add('square-mid');
-	}
-} 
+
 var Squares = {
 
 	init: function() {
@@ -12,26 +7,23 @@ var Squares = {
 		this.btnShowResult = document.getElementById('showResult');
 		this.btnReset = document.getElementById('reset');
 		this.square = [];
-		this.sqr = [];
 		for (var i = 0; i < 100; i++) {
 			this.square[i] = document.createElement('div');
-			this.square[i].id = 'square' + i;
+			this.square[i].dataset.attr = 0;
+			this.square[i].id = i;
 			this.square[i].className = 'square';
 			this.container.appendChild(this.square[i]);
-			
 		}
 		this.events();
 	},
 	events: function() {
 		for (var i = 0; i < 100; i++) {
-			Squares.square[i].addEventListener('click', function() {
-				Squares.sqr[i] = new Square(this, 2);
-				
-				Squares.sqr[i].paint();
-			});
-	    }
+			Squares.square[i].addEventListener('click', Squares.clickSquare);
+		}
 	},
-	clickSquare: function(i) {
+	clickSquare: function(e) {
+		var currentCount = parseInt(e.target.dataset.attr);
+		e.target.dataset.attr = ++currentCount;
 	}
 };
 

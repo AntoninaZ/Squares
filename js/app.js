6cd88f0798d3552d48c4f3aec30f04d164471a07
Antonina Zhuk <a.zhuk@astoundcommerce.com>
2017-06-26 18:58:33 +0300

    added some changes

--- js/app.js
+++ js/app.js
@@ -6,22 +6,48 @@ var Squares = {
 		this.btnGenerateClick = document.getElementById('generateClick');
 		this.btnShowResult = document.getElementById('showResult');
 		this.btnReset = document.getElementById('reset');
-		this.square = [];
-		for (var i = 0; i < 100; i++) {
-			this.square[i] = document.createElement('div');
-			this.square[i].dataset.attr = 0;
-			this.square[i].id = i;
-			this.square[i].className = 'square';
-			this.container.appendChild(this.square[i]);
-		}
+
+		this.generateSquares();
+		this.square = document.getElementsByClassName('square');
+		console.log(this.square[6]);
 		this.events();
 	},
-	events: function() {
+	generateSquares: function() {
 		for (var i = 0; i < 100; i++) {
-			Squares.square[i].addEventListener('click', Squares.clickSquare);
+			var square = document.createElement('div');
+			square.dataset.attr = 0;
+			square.id = i;
+			square.className = 'square';
+			Squares.container.appendChild(square);
 		}
 	},
-	clickSquare: function(e) {
+	events: function() {
+		Squares.btnGenerateClick.addEventListener('click', Squares.generateClickSquare);
+		Squares.btnShowResult.addEventListener('click', Squares.showResult);
+		Squares.btnReset.addEventListener('click', Squares.reset);
+		for (var i = 0; i < 100; i++) {
+			Squares.square[i].addEventListener('click', Squares.userClickSquare);
+		}
+	},
+	generateClickSquare: function() {
+		for (var i = 0; i < 100; i++) {
+			var r = Math.floor((Math.random() * 100));
+			var currentCount = parseInt(document.getElementById(r).getAttribute('data-attr'));
+			document.getElementById(r).setAttribute('data-attr', ++currentCount);
+		}
+	},
+	showResult: function() {
+		for (var i = 0; i < 100; i++) {
+			document.getElementById(i).innerHTML = document.getElementById(i).getAttribute('data-attr');
+		}
+	},
+	reset: function() {
+		for (var i = 0; i < 100; i++) {
+			document.getElementById(i).setAttribute('data-attr', 0); 
+		}
+		Squares.showResult;
+	},
+	userClickSquare: function(e) {
 		var currentCount = parseInt(e.target.dataset.attr);
 		e.target.dataset.attr = ++currentCount;
 	}
